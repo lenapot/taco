fb1483996f9c31831b6ea631387ed70de837c308
gorshkova <gorshkova@tecforce.ru>
2020-05-11 14:05:06 +0400

    Revert "First commit"
    This reverts commit 5e85a4a2

--- src/main/java/tacos/data/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package tacos.data;
-
-import org.springframework.data.repository.CrudRepository;
-import tacos.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-
-    Role findByName(String name);
-}
